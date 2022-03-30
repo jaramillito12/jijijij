a0c29bbe906215307d7ee74e142b1eecc9bd3671
jaramillo <jaramillito098x@gmail.com>
2022-03-30 17:30:41 -0500

    abonos.html

---
+++ abonos.js
@@ -0,0 +1,33 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(event){
+    alert("hiciste click")
+
+let tribuna=document.getElementById("tribuna").value
+    switch(selector){
+        case "1" :
+
+        alert("elegiste la tribuna sur")
+        break
+
+        case "2" :
+        alert("elegiste la tribuna norte")
+        break
+
+        case "3" :
+        alert("elegiste la tribuna occidental")
+        break
+
+        case "4" :
+        alert("elegiste la tribuna vip")
+        break
+
+
+        case "5" :
+        alert("elegiste la tribuna oriental")
+        break
+
+
+
+    }
+
+})
